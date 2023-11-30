6f07b27c9c4ee97114f19f61f14cfb025ec64faf
sasa-sabi <00000sabakan00000@gmail.com>
2023-12-01 01:42:58 +0900

    code open update

--- javascript/code_hide.js
+++ javascript/code_hide.js
@@ -1,8 +1,33 @@
 function toggleContent(contentId) {
   var content = document.getElementById(contentId);
-  if (content.style.display === "none") {
-    content.style.display = "block";
-  } else {
-    content.style.display = "none";
-  }
+  content.classList.toggle("codeButton_hide")
+  content.classList.toggle("code_open");
+}
+
+function all_content_open() {
+  var content = document.querySelectorAll(".code_content");
+  var button = document.getElementById("code_button");
+  var all_button=document.querySelectorAll(".code_all_button")
+
+  content.forEach(function (element) {
+    if (button.classList.contains("codeButton_hide")) {
+      element.classList.remove("codeButton_hide");
+      element.classList.remove("code_open");
+      element.classList.add("codeButton_hide");
+    }
+    else {
+      element.classList.remove("codeButton_hide");
+      element.classList.remove("code_open");
+      element.classList.add("code_open");
+    }
+  });
+  button.classList.toggle("codeButton_hide");
+  all_button.forEach(function (element) {
+    element.classList.toggle("on");
+    if (element.classList.contains("on")) {
+      element.textContent = "コードの非表示";
+    } else {
+      element.textContent = "コードの表示";
+    }
+  });
 }
